4ff8bea14dfaa3585286b941ca4ee40a6947131f
Lily Foster <lily@lily.flowers>
2021-01-03 13:52:15 -0500

    use passwd command to ensure otp is used if available

--- pwchange/ipa.py
+++ pwchange/ipa.py
@@ -8,11 +8,13 @@ def pwchange(username, password, new_password, otp=None):
     client = python_freeipa.ClientMeta(config.ipa_server, verify_ssl=config.verify_ssl)
 
     try:
-        client.change_password(username=username, new_password=new_password, old_password=password, otp=otp)
-    except python_freeipa.exceptions.PWChangeInvalidPassword as err:
+        client.login(username, password + (otp if otp else ''))
+        client.passwd(a_principal=username, a_password=new_password, a_current_password=password)
+        client.logout()
+    except python_freeipa.exceptions.Unauthorized as err:
         raise NameError('Unknown user or invalid password or one-time-password for username {}'.format(repr(username))) from err
-    except python_freeipa.exceptions.PWChangePolicyError as err:
-        raise ValueError('Password does not meet policy requirements: {}'.format(err.policy_error)) from err
+    except python_freeipa.exceptions.BadRequest as err:
+        raise ValueError('Password does not meet policy requirements: {}'.format(err)) from err
     except python_freeipa.exceptions.FreeIPAError as err:
         raise RuntimeError('Unexpected FreeIPA error: {}'.format(err)) from err
     except Exception as err:
